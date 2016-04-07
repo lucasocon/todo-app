f1420c6893b7b08b41748fa21c810faf87881b66
Lucas Ocon <lucas_masterclas@hotmail.com>
2016-04-07 12:31:24 -0300

    Add user accounts & remove insecure package

--- client/main.js
+++ client/main.js
@@ -13,11 +13,7 @@ Template.main.helpers({
 Template.main.events({
   "submit .new-todo": function (event) {
     var text = event.target.text.value;
-
-    Todos.insert({
-      text: text,
-      createdAt: new Date()
-    });
+    Meteor.call('addTodo', text);
 
     // Clear Form
     event.target.text.value = "";
@@ -26,11 +22,15 @@ Template.main.events({
     return false;
   },
   "click .toggle-checked": function (event){
-    Todos.update(this._id, {$set: {checked: ! this.checked}});
+    Meteor.call('setChecked', this._id, !this.checked);
   },
   "click .delete-todo": function () {
     if (confirm("Are you sure?")) {
-      Todos.remove(this._id);
+      Meteor.call('deleteTodo', this._id);
     }
   }
 });
+
+Accounts.ui.config({
+  passwordSignupFields: "USERNAME_ONLY"
+})
